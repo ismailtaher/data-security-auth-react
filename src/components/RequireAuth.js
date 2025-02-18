ac78421800007adfa4f18329d2598d342e983714
ismailtaher <ismail.taher@ymail.com>
2025-02-18 19:13:33 +0500

    removed roles from auth & instead decoded them from accessToken

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,13 +1,23 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import { jwtDecode } from "jwt-decode";
 
 const RequireAuth = ({ allowedRoles }) => {
   const { auth } = useAuth();
   const location = useLocation();
 
-  return auth?.roles?.find((role) => allowedRoles?.includes(role)) ? (
+  if (!auth?.accessToken) {
+    // Wait for auth state to update before making a decision
+    return <div>Loading...</div>;
+  }
+
+  const decoded = auth?.accessToken ? jwtDecode(auth?.accessToken) : undefined;
+
+  const roles = decoded?.UserInfo?.roles || [];
+
+  return roles?.find((role) => allowedRoles?.includes(role)) ? (
     <Outlet />
-  ) : auth?.user ? (
+  ) : auth?.accessToken ? (
     <Navigate to="/unauthorized" state={{ from: location }} replace />
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
